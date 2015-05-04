e7214de5b24cab0b6ee0df7d2174edbef1bd03e9
Maya Minatsuki <mayaeh@marimo-net.org>
2015-05-04 23:37:57 +0900

    Adding create table statement.

--- create_table.sql
+++ create_table.sql
@@ -2,3 +2,71 @@ CREATE TABLE user_id (
 user_id INTEGER PRIMARY KEY AUTOINCREMENT, 
 user_name TEXT 
 ) ;
+
+CREATE TABLE user_pw ( 
+user_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+user_pw_h TEXT 
+) ;
+
+CREATE TABLE user_level ( 
+user_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+authority_level INTEGER　
+) ;
+
+CREATE TABLE authority_level ( 
+level_id INTEGER PRIMARY KEY, 
+authority_name TEXT 
+) ;
+
+CREATE TABLE money_count ( 
+count_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+dt_utc TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+count_1 INTEGER, 
+count_5 INTEGER, 
+count_10 INTEGER, 
+count_50 INTEGER, 
+count_100 INTEGER, 
+count_500 INTEGER, 
+count_1000 INTEGER, 
+count_5000 INTEGER, 
+count_10000 INTEGER, 
+total INTEGER 
+) ;
+
+CREATE TABLE payment_history ( 
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+dt_utc TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+ammount INTEGER, 
+note TEXT 
+) ;
+
+CREATE TABLE payment_text ( 
+id INTEGER PRIMARY KEY, 
+note TEXT 
+) ;
+
+CREATE TABLE receive_history ( 
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+dt_utc TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+ammount INTEGER, 
+note TEXT 
+) ;
+
+CREATE TABLE receive_text ( 
+id INTEGER PRIMARY KEY, 
+note TEXT 
+) ;
+
+CREATE TABLE balance_history ( 
+dt_utc TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+balance INTEGER 
+) ;
+
+CREATE TRIGGER insert_payment_t 
+INSERT ON payment_history BEGIN
+	INSERT INTO balance_history (balance) 
+	VALUES ( 
+	SELECT SUM (ammount) FROM payment_history 
+	WHERE dt_utc < DATETIME ('now') 
+	) ;
+END ;
