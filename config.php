526e18f5a946f94c012f7ec34267e14d7a311e46
Maya Minatsuki <mayaeh@marimo-net.org>
2015-05-06 16:36:56 +0900

    環境設定項目を追加

--- config.php
+++ config.php
@@ -3,11 +3,8 @@
 //  Cashbook
 //  用 config ファイル
 // made this file : 2015.05.04
-// last mod. : 2015.05.04
+// last mod. : 2015.05.06
 
-
-// ページタイトルを設定
-define ('PAGE_TITLE', "My Page") ;
 
 
 
@@ -16,15 +13,39 @@ date_default_timezone_set ( 'Asia/Tokyo' ) ;
 
 
 // 環境設定ファイルからスクリプト設置ディレクトリの設定を読み込む
-include ( 'environment.inc.php' ) ;
+if ( is_readable ('environment.inc.php') ) {
+	include ('environment.inc.php') ;
+}
 
-if ( ! defined ( 'SCRIPT_DIR' ) ) {
+if ( ! defined ('SCRIPT_DIR') ) {
 	// スクリプト設置ディレクトリ
-	define ( 'SCRIPT_DIR' , "./" ) ;
+	define ('SCRIPT_DIR', "./") ;
+}
+
+if ( ! defined ('PAGE_TITLE') ) {
+	// ページタイトルを設定
+	define ('PAGE_TITLE', "My Page") ;
+}
+
+if ( ! defined ('SITE_URL') ) {
+	// URL
+	define ('SITE_URL', "http://localhost/cashbook") ;
+}
+
+if ( ! defined ('PW_HASH_COUNT') ) {
+	define ('PW_HASH_COUNT', 11) ;
+}
+
+if ( ! defined ('LOGIN_SAVE_DATE') ) {
+	// ログイン状態保持日数
+	define ('LOGIN_SAVE_DATE', 3) ;
 }
 
 // file_get_contents でのアクセス時用にユーザーエージェントを設定
-ini_set ( 'user_agent' , 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET4.0C)' ) ;
+ini_set ( 'user_agent', 
+	'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; ' . 
+	'Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; ' . 
+	'.NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET4.0C)' ) ;
 
 // 書き出すディレクトリのパス
 //
