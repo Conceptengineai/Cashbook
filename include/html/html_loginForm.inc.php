6be86ff4615ffcf370d586a63b6267852b5e0c0f
Maya Minatsuki <mayaeh@marimo-net.org>
2015-05-07 03:34:44 +0900

    ログイン用 HTML 部を追加

---
+++ include/html/html_loginForm.inc.php
@@ -0,0 +1,7 @@
+	<div id="loginFormContainer">
+		<form method="post" action="login.php">
+			<input type="text" name="user_name" size="30" id="user_name_text" placeholder="ユーザー名 (半角)" />
+			<input type="password" name="user_pw" size="30" id="user_pw_text" placeholder="パスワード (半角)" />
+			<input type="submit" name="submit_btn" id="submit_btn" value="ログイン" />
+		</form>
+	</div>
