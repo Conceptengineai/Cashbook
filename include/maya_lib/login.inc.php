55c2de202b106e421a6e8fbf66be0b7e95824cb9
Maya Minatsuki <mayaeh@marimo-net.org>
2015-05-07 16:20:29 +0900

    正常ログイン受領時にセッションにユーザー名等を保存する処理を追加

--- include/maya_lib/login.inc.php
+++ include/maya_lib/login.inc.php
@@ -25,8 +25,41 @@ function login ($user_name, $pw) {
 
 		if (password_verify ($pw, $db_pw_h) ) {
 
+
+			$query = 'SELECT screen_name FROM ' . 
+				"user WHERE user_name = '" . 
+				$user_name . "' ;" ;
+
+			$db_screen_name = 
+				$db -> querySingle ($query) ;
+
+// for debug
+//return session_status ();
+//return $db_screen_name ;
+
+			if (session_status () ) {
+			// PHP_SESSION_ACTIVE	: 2
+			// PHP_SESSION_NONE		: 1
+			// PHP_SESSION_DISABLED	: 0
+
+				// セッションハイジャック対策
+				session_regenerate_id (true) ;
+
+				$_SESSION ['user_name'] = 
+					$user_name ;
+
+				if ($db_screen_name) {
+					$_SESSION ['screen_name'] = 
+						$db_screen_name ;
+				}
+
+// for debug
+return 1;
+
+			}
+
 // for debug			
-return 1 ;
+//return 1 ;
 
 		}
 	}
