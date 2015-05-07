1a1b2a706c70dd1a9c2d17f38283174cc0c885c7
Maya Minatsuki <mayaeh@marimo-net.org>
2015-05-07 16:24:19 +0900

    ログイン済の判断に用いるセッション変数を変更

--- index.php
+++ index.php
@@ -19,11 +19,11 @@ if ($_GET) {
 
 // for dump
 //var_dump ( login ('admin', 'fg02v8dFsEja') ) ;
-
+//var_dump ( session_status() );
 
 require_once (HTML_HEADER_FILE) ;
 
-if ( array_get_value ($_SESSION, 'logined', "") ) {
+if ( array_get_value ($_SESSION, 'user_name', "") ) {
 	require_once (HTML_USERFORM_FILE ) ;
 } else {
 	require_once (HTML_LOGINFORM_FILE) ;
