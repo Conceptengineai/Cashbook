b688f779240faa8abd3279b454397e82cf942a25
Maya Minatsuki <mayaeh@marimo-net.org>
2015-05-12 22:10:27 +0900

    ログイン成功時に login_hitsory テーブルに記録する処理を追加 $message 変数に関するワーニングを抑制

--- login.php
+++ login.php
@@ -2,12 +2,20 @@
 
 require_once ('config.php') ;
 
+
+$message = null ;
+
 if ($_POST) {
 
 //var_dump ($_POST) ;
 
 	if (array_get_value ($_POST, 'user_name', "") and 
 		array_get_value ($_POST, 'user_pw', "") ) {
+
+// for debug
+// var_dump ( htmlspecialchars (login (sqlite_escape_string 
+// ($_POST ['user_name']), $_POST ['user_pw']) ) ) ;
+// exit ;
 
 		if (login (sqlite_escape_string 
 			($_POST ['user_name']), 
